ca00e2a02578119dd8d44031ff6da853a7ea7f78
Romain Parage <rparage@talentsoft.com>
2017-03-15 18:06:55 +0100

    Create basic index

---
+++ index.android.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+  <Text>Some text 3</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
