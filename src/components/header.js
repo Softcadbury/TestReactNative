723078dc95146daf7c0d1605060da1af81201fe1
Romain Parage <rparage@talentsoft.com>
2017-03-16 11:12:11 +0100

    Use view to structure the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-    const { textStyle } = styles;
-    return <Text style={textStyle}>Albums</Text>
+    const { viewStyle, textStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    );
 };
 
 const styles = {
+    viewStyle: {
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
     textStyle: {
         fontSize: 20
     }
