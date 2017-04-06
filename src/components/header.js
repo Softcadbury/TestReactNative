0fb2fb4b85d7ba8c985fc32368d1ad6106087cef
Romain Parage <rparage@talentsoft.com>
2017-04-06 18:12:05 +0200

    Add AlbumDetail and Card components

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Header = (props) => {
-    const { viewStyle, textStyle } = styles;
-
     return (
         <View style={viewStyle}>
             <Text style={textStyle}>{props.headerText}</Text>
@@ -11,21 +9,20 @@ const Header = (props) => {
     );
 };
 
-const styles = {
-    viewStyle: {
-        backgroundColor: 'red',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 60,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        elevation: 2,
-        position: 'relative'
-    },
-    textStyle: {
-        fontSize: 20
-    }
+const viewStyle = {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+};
+
+const textStyle = {
+    fontSize: 20
 };
 
 export default Header;
