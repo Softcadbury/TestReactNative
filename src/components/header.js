b540c6ea96eb4ee16a2e253f9a47cf2cc5192d62
Romain Parage <rparage@talentsoft.com>
2017-04-07 10:13:16 +0200

    Move styles in specific const

--- src/components/header.js
+++ src/components/header.js
@@ -3,26 +3,27 @@ import { View, Text } from 'react-native';
 
 const Header = (props) => {
     return (
-        <View style={viewStyle}>
-            <Text style={textStyle}>{props.headerText}</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>{props.headerText}</Text>
         </View>
     );
 };
 
-const viewStyle = {
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 60,
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative'
-};
-
-const textStyle = {
-    fontSize: 20
+const styles = {
+    viewStyle: {
+        backgroundColor: '#2d862d',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 export default Header;
