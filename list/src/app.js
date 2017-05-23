e15b79cd1bc056752cf0848933697dd9e065c188
Romain Parage <rparage@talentsoft.com>
2017-05-23 14:14:10 +0200

    Implement redux to display a list

--- list/src/app.js
+++ list/src/app.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header, Button, Card, CardSection, Spinner } from './components/common';
+import { Header } from './components/common';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import LibraryList from './components/LibraryList';
 
 class App extends Component {
     render() {
         return (
-            <View>
-                <Header headerText='Authentication'></Header>
-            </View>
+            <Provider store={createStore(reducers)}>
+                <View style={{ flex: 1 }}>
+                    <Header headerText='List'></Header>
+                    <LibraryList></LibraryList>
+                </View>
+            </Provider>
         );
     }
 }
