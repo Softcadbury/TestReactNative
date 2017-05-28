18896d19867693ea6c618c231a50f57238a89add
Softcadbury <romain.parage@gmail.com>
2017-05-28 20:21:21 +0200

    Add login form

--- navigation/App.js
+++ navigation/App.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm'
 
 export default class App extends Component {
-  componentWillMount() {
-    var config = {
-      apiKey: "xxxxxxxxxx",
-      authDomain: "xxxxxxxxxx",
-      databaseURL: "xxxxxxxxxx",
-      projectId: "xxxxxxxxxx",
-      storageBucket: "xxxxxxxxxx",
-      messagingSenderId: "xxxxxxxxxx"
-    };
+    componentWillMount() {
+        var config = {
+            apiKey: "xxxxxxxx",
+            authDomain: "xxxxxxxx",
+            databaseURL: "xxxxxxxx",
+            projectId: "xxxxxxxx",
+            storageBucket: "xxxxxxxx",
+            messagingSenderId: "xxxxxxxx"
+        };
 
-    firebase.initializeApp(config);
-  }
+        firebase.initializeApp(config);
+    }
 
-  render() {
-    return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <View style={style.main}>
+                <Provider store={createStore(reducers)}>
+                    <LoginForm />
+                </Provider>
+            </View>
+        );
+    }
+}
+
+const style = {
+    main: {
+        flex: 1,
+        paddingTop: 20
+    }
 }
