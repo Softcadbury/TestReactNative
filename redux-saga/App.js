e9513613f055e3b2267a07df05cb4f4b766a307b
Romain Parage <rparage@talentsoft.com>
2017-07-21 18:05:05 +0200

    Add basic app structure

--- redux-saga/App.js
+++ redux-saga/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import reducers from './src/reducers';
+import sagas from './src/sagas';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        const sagaMiddleware = createSagaMiddleware()
+        const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+        sagaMiddleware.run(sagas)
+
+        return (
+            <View style={style.main}>
+                <Provider store={store}>
+                    <Text>Hello</Text>
+                </Provider>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = {
+    main: {
+        flex: 1,
+        paddingTop: 20
+    }
+}
