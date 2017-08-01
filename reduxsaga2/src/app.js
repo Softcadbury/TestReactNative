91066c0022a6bc7827c9e6be0ef655b5270e37e2
Romain Parage <rparage@talentsoft.com>
2017-08-01 15:44:19 +0200

    Move users code in component

--- reduxsaga2/src/app.js
+++ reduxsaga2/src/app.js
@@ -1,77 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, Image, Dimensions } from 'react-native';
+import { View } from 'react-native';
 
-import Card from './components/Card';
-import CardSection from './components/CardSection';
+import Users from './components/Users';
 
 export default class App extends Component {
-    state = {
-        users: [],
-        currentPage: 0,
-        isLoading: false,
-        areAllLoaded: false
-    };
-
-    fetchUsers() {
-        if (this.state.isLoading || this.state.areAllLoaded) {
-            return;
-        }
-
-        this.setState(
-            {
-                currentPage: this.state.currentPage + 1,
-                isLoading: true
-            },
-            _ => {
-                fetch('https://reqres.in/api/users?page=' + this.state.currentPage).then(response => response.json()).then(responseData => {
-                    console.log( this.state.currentPage , responseData.total_pages);
-
-                    this.setState({
-                        users: [...this.state.users, ...responseData.data],
-                        isLoading: false,
-                        areAllLoaded: this.state.currentPage == responseData.total_pages
-                    });
-                });
-            }
-        );
-    }
-
-    componentWillMount() {
-        this.fetchUsers();
-    }
-
-    onScroll(e) {
-        var windowHeight = Dimensions.get('window').height;
-        var height = e.nativeEvent.contentSize.height;
-        var offset = e.nativeEvent.contentOffset.y;
-        if (windowHeight + offset >= height) {
-            this.fetchUsers();
-        }
-    }
-
-    renderUsers() {
-        return this.state.users.map(user =>
-            <Card key={user.id}>
-                <CardSection>
-                    <Image style={style.image} source={{ uri: user.avatar }} />
-                </CardSection>
-                <CardSection>
-                    <View key={user.id}>
-                        <Text style={style.name}>
-                            {user.first_name} {user.last_name}
-                        </Text>
-                    </View>
-                </CardSection>
-            </Card>
-        );
-    }
-
     render() {
         return (
             <View style={style.main}>
-                <ScrollView onScroll={this.onScroll.bind(this)}>
-                    {this.renderUsers()}
-                </ScrollView>
+                <Users />
             </View>
         );
     }
@@ -81,14 +17,5 @@ const style = {
     main: {
         flex: 1,
         paddingBottom: 10
-    },
-    image: {
-        height: 200,
-        flex: 1,
-        width: null
-    },
-    name: {
-        margin: 10,
-        fontSize: 20
     }
 };
---
+++ reduxsaga2/src/components/Users.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Text, ScrollView, Image, Dimensions } from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
+
+export default class Users extends Component {
+    state = {
+        users: [],
+        currentPage: 0,
+        isLoading: false,
+        areAllLoaded: false
+    };
+
+    fetchUsers() {
+        if (this.state.isLoading || this.state.areAllLoaded) {
+            return;
+        }
+
+        this.setState(
+            {
+                currentPage: this.state.currentPage + 1,
+                isLoading: true
+            },
+            _ => {
+                fetch('https://reqres.in/api/users?page=' + this.state.currentPage).then(response => response.json()).then(responseData => {
+                    this.setState({
+                        users: [...this.state.users, ...responseData.data],
+                        isLoading: false,
+                        areAllLoaded: this.state.currentPage == responseData.total_pages
+                    });
+                });
+            }
+        );
+    }
+
+    componentWillMount() {
+        this.fetchUsers();
+    }
+
+    onScroll(e) {
+        var windowHeight = Dimensions.get('window').height;
+        var height = e.nativeEvent.contentSize.height;
+        var offset = e.nativeEvent.contentOffset.y;
+
+        if (windowHeight + offset >= height) {
+            this.fetchUsers();
+        }
+    }
+
+    renderUsers() {
+        return this.state.users.map(user =>
+            <Card key={user.id}>
+                <CardSection>
+                    <Image style={style.image} source={{ uri: user.avatar }} />
+                </CardSection>
+                <CardSection>
+                    <Text style={style.name}>
+                        {user.first_name} {user.last_name}
+                    </Text>
+                </CardSection>
+            </Card>
+        );
+    }
+
+    render() {
+        return (
+            <ScrollView onScroll={this.onScroll.bind(this)}>
+                {this.renderUsers()}
+            </ScrollView>
+        );
+    }
+}
+
+const style = {
+    image: {
+        height: 200,
+        flex: 1,
+        width: null
+    },
+    name: {
+        margin: 10,
+        fontSize: 20
+    }
+};
